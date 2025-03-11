bf93cc5cbb6ee7c10e3206a8bf3f7ae6a41f429c
dudaordahy <dudasciortino@gmail.com>
2025-03-11 10:01:04 -0300

    atualizações

---
+++ testes js/script.js
@@ -0,0 +1,8 @@
+document.getElementById('meuBotao').addEventListener('click',
+function() {
+    alert("Botão Clicado");
+});
+
+function mudarTexto(){
+    document.getElementById("mensagem").innerHTML = "Texto alterado!"
+};
